5f36b97e06541d1086cc2047a16dcb256992ef88
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2016-04-11 10:38:27 -0300

    Create a chat example

---
+++ chat.js
@@ -0,0 +1,41 @@
+const streamer = new Meteor.Streamer('chat');
+
+if(Meteor.isClient) {
+	const messages = new Mongo.Collection(null);
+
+	window.sendMessage = function(text) {
+		streamer.emit('message', {
+			type: 'user',
+			user: Meteor.user() ? Meteor.user().username : 'anonymous',
+			text: text
+		});
+		messages.insert({
+			type: 'self',
+			text: text
+		});
+	};
+
+	streamer.on('message', function(message) {
+		messages.insert(message);
+	});
+
+	Template.body.events({
+		'keydown input'(e) {
+			if (e.which === 13) {
+				window.sendMessage(e.target.value);
+				e.target.value = '';
+			}
+		}
+	});
+
+	Template.body.helpers({
+		messages() {
+			return messages.find();
+		}
+	});
+}
+
+if (Meteor.isServer) {
+	streamer.allowRead('all');
+	streamer.allowWrite('all');
+}
